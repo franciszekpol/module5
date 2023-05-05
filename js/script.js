adc32d4028b48e53edc30255e3e04f1d4a5c9345
Franciszek Pol <franciszekpol@outlook.com>
2023-05-05 11:50:47 +0200

    Remove class 'active' from all articles

--- js/script.js
+++ js/script.js
@@ -6,12 +6,22 @@ function titleClickHandler(event) {
 }
 
 /* remove class 'active' from all article links */
+const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks) {
+    activeLink.classList.remove('active');
+}
 
 /* add class 'active' to the clicked link */
 
 /* remove class 'active' from all articles  */
+const activeArticles = document.querySelectorAll('.post.active');
 
-/* get 'href' attrivute from the clicked link */
+for(let activeArticle of activeArticles) {
+    activeArticle.classList.remove('active');
+}
+
+/* get 'href' attribute from the clicked link */
 
 /* find the correct article using the selector (value of 'href' attribute) */
 
